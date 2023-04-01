b2fef6d547007bef22f7438a8b4ae409cd46447c
Venkysanju246 <sanjuvenky246@gmail.com>
2023-04-01 21:16:19 +0530

    done day5

--- kindmeal/backend/controller/product.controller.js
+++ kindmeal/backend/controller/product.controller.js
@@ -2,11 +2,17 @@ const express = require("express")
 const ProductModel = require("../model/products.route")
 const productRouter = express.Router()
 
-productRouter.post("/add",async (req, res)=>{
+productRouter.post("/add", async (req, res) => {
    const newData = await ProductModel.insertMany(req.body)
-         res.send({
-            msg:"Yahoo!, Product added suceesfully"
-         })
+   res.send({
+      msg: "Yahoo!, Product added suceesfully"
+   })
+})
+productRouter.get("/get", async (req, res) => {
+   const newData = await ProductModel.find()
+   res.send({
+      data: newData
+   })
 })
 
 module.exports = productRouter
